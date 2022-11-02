5730adfb5fc510d8067b6d3ab10e9db6b0345ab4
Djordje <djordjemilic33@gmail.com>
2022-11-02 14:42:18 +0100

    Added auth routes and auth controller and auth service

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,5 +1,14 @@
 import express from "express";
+import { createSessionHandler } from "../controller/auth.controller";
+import validateResource from "../middleware/validate-resource";
+import { createSessionSchema } from "../schema/auth.schema";
 
 const router = express.Router();
+
+router.post(
+  "/api/sessions",
+  validateResource(createSessionSchema),
+  createSessionHandler
+);
 
 export default router;
--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -17,16 +17,16 @@ export function signJwt(
   });
 }
 
-export function verifyJwt(
+export function verifyJwt<T>(
   token: string,
   keyName: "accessTokenPublicKey" | "refreshTokenPublicKey"
-) {
+): T | null {
   const publicKey = Buffer.from(config.get<string>(keyName), "base64").toString(
     "ascii"
   );
 
   try {
-    const decoded = jwt.verify(token, publicKey);
+    const decoded = jwt.verify(token, publicKey) as T;
     return decoded;
   } catch (e) {
     return null;
